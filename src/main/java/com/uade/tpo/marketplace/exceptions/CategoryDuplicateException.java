bfa5277967259d21a5dda7ca57d44bc8479e2e23
chiara-p581 <chiaraagustina19@gmail.com>
2025-08-30 23:54:04 -0300

    Delete src directory

--- src/main/java/com/uade/tpo/marketplace/exceptions/CategoryDuplicateException.java
+++
@@ -1,8 +0,0 @@
-package com.uade.tpo.marketplace.exceptions;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "La categoria que se intenta agregar esta duplicada")
-public class CategoryDuplicateException extends Exception {
-}
