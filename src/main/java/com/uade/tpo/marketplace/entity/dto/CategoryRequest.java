c18c7debba06518d48f944c5421308a9020bdf8a
Fer <145226583+vallferre@users.noreply.github.com>
2025-08-31 22:18:04 -0300

    Delete src directory

--- src/main/java/com/uade/tpo/marketplace/entity/dto/CategoryRequest.java
+++
@@ -1,9 +0,0 @@
-package com.uade.tpo.marketplace.entity.dto;
-
-import lombok.Data;
-
-@Data
-public class CategoryRequest {
-    private int id;
-    private String description;
-}
--- src/main/java/com/uade/tpo/marketplace/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.uade.tpo.marketplace.repository;
-
-import com.uade.tpo.marketplace.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
