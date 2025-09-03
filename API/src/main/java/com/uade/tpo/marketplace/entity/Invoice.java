3928cf8363940cb715d904fb6ff93eb764d4e633
agosber <agostinabernard@gmail.com>
2025-09-03 11:21:45 -0300

    Add files via upload

--- API/src/main/java/com/uade/tpo/marketplace/entity/Invoice.java
+++ API/src/main/java/com/uade/tpo/marketplace/entity/Invoice.java
@@ -1,9 +1,14 @@
 package com.uade.tpo.marketplace.entity;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -15,6 +20,22 @@ public class Invoice {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private LocalDate date;
+    private Double total;
+
+    @ManyToOne
+    @JoinColumn(name = "buyer_id", nullable = false)
+    private User buyer;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id", nullable = false)
+    private User seller;
+
+    @OneToOne
+    @JoinColumn(name = "order_id", referencedColumnName = "id")
+    private Order order;
+    
     /* 
     @Column(nullable = false)
     private LocalDateTime issueDate;
