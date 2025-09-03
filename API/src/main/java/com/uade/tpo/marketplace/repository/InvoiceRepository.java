c96c0e078d35e0699969d37d3929526ca06c0100
Agostina Bernard <agostinabernard@MacBook-Air-de-Agostina-2.local>
2025-09-03 11:36:03 -0300

    Cambios en Invoice

---
+++ API/src/main/java/com/uade/tpo/marketplace/repository/InvoiceRepository.java
@@ -0,0 +1,19 @@
+package com.uade.tpo.marketplace.repository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.uade.tpo.marketplace.entity.Invoice;
+
+public interface InvoiceRepository {
+    //buscar facturas por id de buyer
+    List<Invoice> findByBuyerrId(Long buyerId);
+
+    //buscar facturas por id de seller
+    List<Invoice> findBySellerId(Long sellerId);
+
+    //buscar factura por id de orden
+    Invoice findByOrderId(Long orderId);
+
+}
